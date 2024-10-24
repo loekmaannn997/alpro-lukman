6d1977f14d2f7da8b0871ba2e561b484a263abd9
loekmaannn997 <rachmadlukman997@gmail.com>
2024-10-24 15:33:32 +0700

    Add files via upload

---
+++ modul.py
@@ -0,0 +1,12 @@
+import math
+
+akar = math.sqrt(28)
+pangkat = math.pow(5,7)
+bulat_bawah = math.floor(4.7)
+bulat_atas = math.ceil(6.4)
+print()           
+print(f"akar 28 = {akar}")
+print(f"5 pankat 7 = {pangkat}")
+print(f"pembulatan ke bawah dari 4.7 = {bulat_bawah}")
+print(f"pembulatan ke atas dari 6.4 = {bulat_atas}")
+print()
---
+++ perhitungan.py
@@ -0,0 +1,7 @@
+#operasi hitung
+soal_1 = 4 + 3 ** 4 / 2 * 6
+print(f"no 1 = {soal_1}")
+soal_2 = 1 * 4 -3
+print(f"no2 = {soal_2}")
+soal_3 = 5 * 8 /3 + 5
+print(f"no3 = {soal_3}")
